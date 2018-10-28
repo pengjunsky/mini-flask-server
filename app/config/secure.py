0194bc41496313c03c6b1b25e61c692ff3c2335e
PJ <pengjunsky@163.com>
2018-10-28 09:41:58 +0800

    更新

--- app/config/secure.py
+++ app/config/secure.py
@@ -1,3 +1,3 @@
-SQLALCHEMY_DATABASE_URI = 'mysql+cymysql://root:maomao@localhost:3306/mina'
+SQLALCHEMY_DATABASE_URI = 'mysql+cymysql://root:maomao@localhost:3306/mall'
 SECRET_KEY = 'uWVgBuXBpxbt1mxrSypzCmreFDqgo8ma'
 SQLALCHEMY_TRACK_MODIFICATIONS = True
